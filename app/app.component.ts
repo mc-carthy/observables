75d40ab38da9d314f6702b41aa77dbc25988c63a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-23 16:38:20 +0100

    Inducing callback hell

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,29 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <input id="search" type="text" class="form-control">
+        <input id="search" type="text" class="form-control" placeholder="Search">
     `
 })
 export class AppComponent {
     constructor(){
+
+        var debounced = _.debounce(function(text){
+            var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
+            $.getJSON(url, function(artists){
+                console.log(artists);
+            })
+        }, 400);
+
+        $("#search").keyup(function(e){
+            var text = e.target.value;
+
+            if (text.length < 3)
+            {
+                return;
+            }
+
+            debounced(text);
+
+        });
     }
 }
